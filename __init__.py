598b5031cf4fa1a4c821ad2711b76b6bd7a5f84b
Anatolii Veselyi <anatolii.veselyi@WS-M25P25D46G.local>
2023-02-07 02:41:25 +0200

    refactored to refference function

---
+++ __init__.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI = "sqlite:///classes.sqlite",
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    
+    from .model import db, init_db_command
+    db.init_app(app)
+    with app.app_context():
+        app.cli.add_command(init_db_command)
+        
+    from . import routes
+    app.register_blueprint(routes.bp)
+        
+        
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
--- routes.py
+++ routes.py
@@ -1,25 +1,29 @@
-from app import db, app
-from model import L2_classes, Archers, Warriors, Mages, Assassins
+from flask import Blueprint
+from .model import L2_classes, Archers, Warriors, Mages, Assassins
 from flask import render_template
 
-@app.route('/index')
-@app.route('/')
+
+
+bp = Blueprint('routes', __name__, url_prefix='/')
+
+@bp.route('/index')
+@bp.route('/')
 def index():
     return render_template('index.html', template_l2_classes = L2_classes.query.all())
 
 
-@app.route('/Archers')
+@bp.route('/Archers')
 def archers():
     return render_template('classes.html', template_class_name = 'Archers', template_class = Archers.query.all())
 
-@app.route('/Warriors')
+@bp.route('/Warriors')
 def warriors():
     return render_template('classes.html', template_class_name = 'Warriors', template_class = Warriors.query.all())
 
-@app.route('/Mages')
+@bp.route('/Mages')
 def mages():
     return render_template('classes.html', template_class_name = 'Mages', template_class = Mages.query.all())
 
-@app.route('/Assassins')
+@bp.route('/Assassins')
 def assassins():
     return render_template('classes.html', template_class_name = 'Assassins', template_class = Assassins.query.all())
